112b9535edc04b6de10ee8c52d36cdccd2504a54
unknown <yuvarani@MUMBAI-AD.PLURALSIGHT.COM>
2024-07-30 16:20:06 +0530

    Initial commit

---
+++ EmployeesInformation/Controllers/EmployeesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace EmployeesInformation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly EmployeeDataStore _employeedataStore;
+
+        public EmployeesController()
+        {
+            _employeedataStore = new EmployeeDataStore();
+        }
+
+        [HttpGet] // GET: api/employees
+        public JsonResult GetEmployees()
+        {
+            var employees = _employeedataStore.Employees;
+            return new JsonResult(employees);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetEmployee(int id)
+        {
+            var employees = _employeedataStore.Employees.FirstOrDefault(e => e.Id == id);
+            if (employees == null)
+            {
+                return new JsonResult(new { message = "employee not found" }) { StatusCode = 404 };
+            }
+            return new JsonResult(employees);
+        }
+    }
+}
---
+++ EmployeesInformation/EmployeeDataStore.cs
@@ -0,0 +1,20 @@
+﻿using EmployeesInformation.Models;
+
+namespace EmployeesInformation
+{
+    public class EmployeeDataStore
+    {
+        public List<Employee>Employees { get; set; }
+
+        public EmployeeDataStore() {
+            Employees = new List<Employee>
+            {
+                new Employee { Id = 1, Name = "John Doe", JobTitle = "Manager", Salary = 60000 },
+                new Employee { Id = 2, Name = "Jane Smith", JobTitle = "Developer", Salary = 80000 },
+                new Employee { Id = 3, Name = "Mike Johnson", JobTitle = "Analyst", Salary = 70000 },
+                new Employee { Id = 4, Name = "Alice Brown", JobTitle = "Designer", Salary = 75000 },
+                new Employee { Id = 5, Name = "Bob Davis", JobTitle = "Support", Salary = 50000 }
+            };
+        }
+    }
+}
